a62829cbbee66cbbbbae18d2b7076256990bba2d
Kory Maincent <kory.maincent@bootlin.com>
2024-10-16 17:24:57 +0200

    regulator: Add support for power budget description
    In preparation for future support of PSE port priority and power
    management, we need the power budget value of the power supply.
    This addition allows the regulator to track the available power
    capacity, which will be essential for prioritizing ports when
    making power allocation decisions.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -235,6 +235,7 @@ int regulator_sync_voltage(struct regulator *regulator);
 int regulator_set_current_limit(struct regulator *regulator,
 			       int min_uA, int max_uA);
 int regulator_get_current_limit(struct regulator *regulator);
+int regulator_get_power_budget(struct regulator *regulator);
 
 int regulator_set_mode(struct regulator *regulator, unsigned int mode);
 unsigned int regulator_get_mode(struct regulator *regulator);
@@ -530,6 +531,11 @@ static inline int regulator_set_current_limit(struct regulator *regulator,
 }
 
 static inline int regulator_get_current_limit(struct regulator *regulator)
+{
+	return 0;
+}
+
+static inline int regulator_get_power_budget(struct regulator *regulator)
 {
 	return 0;
 }

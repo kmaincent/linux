f07532f66008f60c58f2c41e24551b5c71cd5f0b
Kory Maincent <kory.maincent@bootlin.com>
2025-01-29 17:28:58 +0100

    drm/panel: ilitek-ili9881c: Add support for two-lane configuration
    Enable support for two-lane configuration which is done by setting the
    LANSEL_SW_EN and LANSEL_SW bits in the Pad Control register.
    
    Use the data-lanes device tree parameter to configure the number of lanes.
    The default configuration remains set to four lanes.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>
    ---
    
    Change in v3:
    - Use drm_of_get_data_lanes_count_ep.
    
    Change in v2:
    - Read the data-lanes parameter from the port endpoint and use
      drm_of_get_data_lanes_count instead of of_property_read_u32.

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -16,6 +16,7 @@
 
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
+#include <drm/drm_of.h>
 #include <drm/drm_panel.h>
 
 #include <video/mipi_display.h>
@@ -1263,6 +1264,21 @@ static int ili9881c_send_cmd_data(struct ili9881c *ctx, u8 cmd, u8 data)
 	return 0;
 }
 
+static int ili9881c_set_lanes_cfg(struct ili9881c *ctx)
+{
+	int ret;
+
+	if (ctx->dsi->lanes != 2)
+		/* Nothing to do */
+		return 0;
+
+	ret = ili9881c_switch_page(ctx, 1);
+	if (ret)
+		return ret;
+
+	return ili9881c_send_cmd_data(ctx, 0xB7, 0x3);
+}
+
 static int ili9881c_prepare(struct drm_panel *panel)
 {
 	struct ili9881c *ctx = panel_to_ili9881c(panel);
@@ -1294,6 +1310,10 @@ static int ili9881c_prepare(struct drm_panel *panel)
 		if (ret)
 			return ret;
 	}
+
+	ret = ili9881c_set_lanes_cfg(ctx);
+	if (ret)
+		return ret;
 
 	ret = ili9881c_switch_page(ctx, 0);
 	if (ret)
@@ -1504,7 +1524,7 @@ static const struct drm_panel_funcs ili9881c_funcs = {
 static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
 {
 	struct ili9881c *ctx;
-	int ret;
+	int ret, lanes;
 
 	ctx = devm_kzalloc(&dsi->dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
@@ -1545,11 +1565,17 @@ static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
 	if (ret)
 		return ret;
 
+	lanes = drm_of_get_data_lanes_count_ep(dsi->dev.of_node, -1, -1, 2, 4);
+	if (lanes == -EINVAL)
+		lanes = 4;
+	else if (lanes < 0)
+		return lanes;
+
 	drm_panel_add(&ctx->panel);
 
 	dsi->mode_flags = ctx->desc->mode_flags;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->lanes = 4;
+	dsi->lanes = lanes;
 
 	return mipi_dsi_attach(dsi);
 }

0456b388ea61b6a4def384d5e5bd84026494fbb9
Kory Maincent <kory.maincent@bootlin.com>
2026-04-02 11:39:28 +0200

    drm/mode: Skip NULL destroy callbacks in drm_mode_config_cleanup()
    In drm_mode_config_cleanup(), the destroy callback is unconditionally
    invoked for every plane and CRTC. For DRM-managed resources, this
    callback is no longer needed since cleanup is handled automatically.
    
    When incrementally converting a driver's resources from manually-managed
    to DRM-managed, there is a window where drm_mode_config_cleanup() is
    still called but some destroy callbacks have already been removed. This
    would result in a NULL pointer dereference.
    
    Guard the destroy calls with a NULL check to allow drivers to convert
    their DRM resources one at a time rather than in a single large,
    hard-to-review commit.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- drivers/gpu/drm/drm_mode_config.c
+++ drivers/gpu/drm/drm_mode_config.c
@@ -567,11 +567,13 @@ void drm_mode_config_cleanup(struct drm_device *dev)
 
 	list_for_each_entry_safe(plane, plt, &dev->mode_config.plane_list,
 				 head) {
-		plane->funcs->destroy(plane);
+		if (plane->funcs->destroy)
+			plane->funcs->destroy(plane);
 	}
 
 	list_for_each_entry_safe(crtc, ct, &dev->mode_config.crtc_list, head) {
-		crtc->funcs->destroy(crtc);
+		if (crtc->funcs->destroy)
+			crtc->funcs->destroy(crtc);
 	}
 
 	list_for_each_entry_safe(blob, bt, &dev->mode_config.property_blob_list,
